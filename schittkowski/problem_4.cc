e294503623550cc45587a87150b2bf93d94c4cfb
Thomas Moulard <thomas.moulard@gmail.com>
2013-04-01 22:45:31 +0900

    Add probem 4.

---
+++ schittkowski/problem_4.cc
@@ -0,0 +1,160 @@
+// Copyright (C) 2013 by Thomas Moulard, AIST, CNRS.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "common.hh"
+
+namespace roboptim
+{
+  namespace schittkowski
+  {
+    namespace problem4
+    {
+      struct ExpectedResult
+      {
+	static const double f0;
+	static const double x[];
+	static const double fx;
+      };
+      const double ExpectedResult::f0 = 3.323568;
+      const double ExpectedResult::x[] = {1., 0.};
+      const double ExpectedResult::fx = 8. / 3.;
+
+      template <typename T>
+      class F : public GenericDifferentiableFunction<T>
+      {
+      public:
+	ROBOPTIM_DIFFERENTIABLE_FUNCTION_FWD_TYPEDEFS
+	(GenericDifferentiableFunction<T>);
+
+	explicit F () throw ();
+	void
+	impl_compute (result_t& result, const argument_t& x) const throw ();
+	void
+	impl_gradient (gradient_t& grad, const argument_t& x, size_type)
+	  const throw ();
+      };
+
+      template <typename T>
+      F<T>::F () throw ()
+	: GenericDifferentiableFunction<T>
+	  (2, 1, "⅓ * (x₀ + 1)³ + x₁")
+      {}
+
+      template <typename T>
+      void
+      F<T>::impl_compute (result_t& result, const argument_t& x)
+	const throw ()
+      {
+	result[0] = (1. / 3.) * std::pow (x[0] + 1., 3) + x[1];
+      }
+
+      template <>
+      void
+      F<EigenMatrixSparse>::impl_gradient
+      (gradient_t& grad, const argument_t& x, size_type)
+	const throw ()
+      {
+	grad.insert (0) = std::pow (x[0] + 1, 3);
+	grad.insert (1) = 1;
+      }
+
+      template <typename T>
+      void
+      F<T>::impl_gradient (gradient_t& grad, const argument_t& x, size_type)
+	const throw ()
+      {
+	grad[0] = std::pow (x[0] + 1, 3);
+	grad[1] = 1;
+      }
+    } // end of namespace problem4.
+  } // end of namespace schittkowski.
+} // end of namespace roboptim.
+
+BOOST_FIXTURE_TEST_SUITE (schittkowski, TestSuiteConfiguration)
+
+BOOST_AUTO_TEST_CASE (schittkowski_problem4)
+{
+  using namespace roboptim;
+  using namespace roboptim::schittkowski::problem4;
+
+  typedef Solver<
+    GenericDifferentiableFunction<functionType_t>,
+    boost::mpl::vector<GenericLinearFunction<functionType_t>,
+		       GenericDifferentiableFunction<functionType_t> > >
+    solver_t;
+
+  // Build problem.
+  F<functionType_t> f;
+  typename solver_t::problem_t problem (f);
+
+  problem.argumentBounds ()[0] = F<functionType_t>::makeLowerInterval (1.);
+  problem.argumentBounds ()[1] = F<functionType_t>::makeLowerInterval (0.);
+
+  typename F<functionType_t>::argument_t x (2);
+  x << 1.125, 0.125;
+  problem.startingPoint () = x;
+
+  BOOST_CHECK_CLOSE (f (x)[0], ExpectedResult::f0, 1e-4);
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+  // Initialize solver.
+  SolverFactory<solver_t> factory (SOLVER_NAME, problem);
+  solver_t& solver = factory ();
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+  // Compute the minimum and retrieve the result.
+  typename solver_t::result_t res = solver.minimum ();
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+
+  // Display solver information.
+  std::cout << solver << std::endl;
+
+  // Check if the minimization has succeed.
+  if (res.which () != solver_t::SOLVER_VALUE)
+    {
+      std::cout << "A solution should have been found. Failing..."
+                << std::endl
+                << boost::get<SolverError> (res).what ()
+                << std::endl;
+      BOOST_CHECK_EQUAL (res.which (), solver_t::SOLVER_VALUE);
+      return;
+    }
+
+  // Get the result.
+  Result& result = boost::get<Result> (res);
+
+  // Check final x.
+  for (unsigned i = 0; i < result.x.size (); ++i)
+    BOOST_CHECK_CLOSE (1. + result.x[i], 1. + ExpectedResult::x[i], 1e-4);
+
+  // Check final value.
+  BOOST_CHECK_CLOSE (1. + result.value[0], 1. + ExpectedResult::fx, 1e-4);
+
+
+  // Display the result.
+  std::cout << "A solution has been found: " << std::endl
+	    << result << std::endl;
+}
+
+BOOST_AUTO_TEST_SUITE_END ()
