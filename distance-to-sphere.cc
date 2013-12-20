b74a13877f73cf64655102ac50ed36b74244d558
Benjamin Chrétien <chretien@lirmm.fr>
2013-12-20 01:32:58 +0100

    Add sparse support for distance-to-sphere.

--- distance-to-sphere.cc
+++ distance-to-sphere.cc
@@ -15,54 +15,10 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <iostream>
 
-#include <boost/mpl/list.hpp>
-#include <boost/test/test_case_template.hpp>
-#include <boost/test/unit_test.hpp>
+#include "common.hh"
 
-#include <log4cxx/basicconfigurator.h>
-
-#include <roboptim/core/differentiable-function.hh>
-#include <roboptim/core/io.hh>
-#include <roboptim/core/optimization-logger.hh>
-#include <roboptim/core/solver.hh>
-#include <roboptim/core/solver-factory.hh>
 #include <roboptim/core/sum-of-c1-squares.hh>
-
-#ifndef SOLVER_NAME
-# error "please define solver name"
-#endif //! PROBLEM_TYPE
-
-#ifndef PLUGIN_PATH
-# error "please define plug-in path"
-#endif //! PROBLEM_TYPE
-
-#define FORWARD_TYPEDEFS()                              \
-  typedef GenericDifferentiableFunction<T> parent_t;    \
-  typedef typename parent_t::result_t result_t;         \
-  typedef typename parent_t::size_type size_type;       \
-  typedef typename parent_t::argument_t argument_t;     \
-  typedef typename parent_t::gradient_t gradient_t;     \
-  typedef typename parent_t::jacobian_t jacobian_t
-
-typedef boost::mpl::list< ::roboptim::EigenMatrixDense> functionTypes_t;
-
-struct TestSuiteConfiguration
-{
-  TestSuiteConfiguration ()
-  {
-    log4cxx::BasicConfigurator::configure ();
-
-    lt_dlinit();
-    BOOST_REQUIRE_EQUAL (lt_dlsetsearchpath (PLUGIN_PATH), 0);
-  }
-
-  ~TestSuiteConfiguration ()
-  {
-    lt_dlexit ();
-  }
-};
 
 namespace roboptim
 {
@@ -84,85 +40,128 @@ namespace roboptim
     template <typename T>
     struct F : public GenericDifferentiableFunction<T>
     {
-      FORWARD_TYPEDEFS ();
+      ROBOPTIM_DIFFERENTIABLE_FUNCTION_FWD_TYPEDEFS_
+      (GenericDifferentiableFunction<T>);
 
-      explicit F () : DifferentiableFunction
+      explicit F () : GenericDifferentiableFunction<T>
 		      (2, 3,
 		       "vector between unit sphere and point (x,y,z)"),
 		      point_ (3)
       {
 	sphericalCoordinates (point_, ExpectedResult::x[0],
-				      ExpectedResult::x[1]);
+                              ExpectedResult::x[1]);
 	point_ *= 2.;
       }
 
       ~F () throw ()
       {}
 
-      void impl_compute (result_t& result, const argument_t& x) const throw ()
-      {
-	result.setZero ();
-	double theta = x[0];
-	double phi = x[1];
-	sphericalCoordinates (result, theta, phi);
-	result -= point_;
-      }
-
+      void impl_compute (result_t& result, const argument_t& x) const throw ();
       void impl_gradient (gradient_t& gradient, const argument_t& x,
-			  size_type functionId = 0) const throw ()
-      {
-	double theta = x[0];
-	double phi = x[1];
-	switch (functionId)
-	  {
-	  case 0:
-	    gradient[0] = -sin(theta) * cos(phi);
-	    gradient[1] = -cos(theta) * sin(phi);
-	    break;
-	  case 1:
-	    gradient[0] = cos(theta) * cos(phi);
-	    gradient[1] = -sin(theta) * sin(phi);
-	    break;
-	  case 2:
-	    gradient[0] = 0.;
-	    gradient[1] = cos(phi);
-	    break;
-	  default:
-	    abort();
-	  }
-      }
+                          size_type functionId = 0) const throw ();
+
 
       static void sphericalCoordinates (result_t& res, double theta, double phi)
       {
-	res (0) = cos(theta) * cos(phi);
-	res (1) = sin(theta) * cos(phi);
-	res (2) = sin(phi);
+        res (0) = cos(theta) * cos(phi);
+        res (1) = sin(theta) * cos(phi);
+        res (2) = sin(phi);
       }
       result_t point_;
     };
+
+    template <typename T>
+    void
+    F<T>::impl_compute (result_t& result, const argument_t& x) const throw ()
+    {
+      result.setZero ();
+      double theta = x[0];
+      double phi = x[1];
+      sphericalCoordinates (result, theta, phi);
+      result -= point_;
+    }
+
+    template <>
+    void
+    F<EigenMatrixSparse>::impl_gradient
+    (gradient_t& grad, const argument_t& x, size_type functionId)
+      const throw ()
+    {
+      grad.setZero ();
+
+      double theta = x[0];
+      double phi = x[1];
+
+      switch (functionId)
+        {
+        case 0:
+          grad.insert (0) = -sin(theta) * cos(phi);
+          grad.insert (1) = -cos(theta) * sin(phi);
+          break;
+        case 1:
+          grad.insert (0) = cos(theta) * cos(phi);
+          grad.insert (1) = -sin(theta) * sin(phi);
+          break;
+        case 2:
+          grad.insert (0) = 0.;
+          grad.insert (1) = cos(phi);
+          break;
+        default:
+          abort();
+        }
+    }
+
+    template <typename T>
+    void
+    F<T>::impl_gradient
+    (gradient_t& grad, const argument_t& x, size_type functionId)
+      const throw ()
+    {
+      grad.setZero ();
+
+      double theta = x[0];
+      double phi = x[1];
+
+      switch (functionId)
+        {
+        case 0:
+          grad[0] = -sin(theta) * cos(phi);
+          grad[1] = -cos(theta) * sin(phi);
+          break;
+        case 1:
+          grad[0] = cos(theta) * cos(phi);
+          grad[1] = -sin(theta) * sin(phi);
+          break;
+        case 2:
+          grad[0] = 0.;
+          grad[1] = cos(phi);
+          break;
+        default:
+          abort();
+        }
+    }
+
   } // namespace distanceToSphere
 } // namespace roboptim
 
 
 BOOST_FIXTURE_TEST_SUITE (distanceToSphere, TestSuiteConfiguration)
 
-BOOST_AUTO_TEST_CASE_TEMPLATE (distanceToSphere_problem1, T, functionTypes_t)
+BOOST_AUTO_TEST_CASE (distanceToSphere_problem1)
 {
   using namespace roboptim;
   using namespace roboptim::distanceToSphere;
-
-  typedef Solver<SumOfC1Squares, boost::mpl::vector<> > solver_t;
 
   // Check tolerance
   double check_tol = 1e-2;
 
   // Build problem.
-  boost::shared_ptr <F<T> > f (new F<T> ());
-  SumOfC1Squares soq (f, "");
+  boost::shared_ptr <F<functionType_t> > f (new F<functionType_t> ());
+  GenericSumOfC1Squares<functionType_t> soq (f, "");
 
-  typename solver_t::problem_t problem (soq);
+  solver_t::problem_t problem (soq);
 
-  typename F<T>::argument_t x (2);
+  F<functionType_t>::argument_t x (2);
   x << ExpectedResult::x0[0], ExpectedResult::x0[1];
   problem.startingPoint () = x;
 
@@ -177,7 +176,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE (distanceToSphere_problem1, T, functionTypes_t)
     (solver, "/tmp/roboptim-shared-tests/" SOLVER_NAME "/distance-to-sphere");
 
   // Compute the minimum and retrieve the result.
-  typename solver_t::result_t res = solver.minimum ();
+  solver_t::result_t res = solver.minimum ();
 
   // Display solver information.
   std::cout << solver << std::endl;
@@ -191,7 +190,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE (distanceToSphere_problem1, T, functionTypes_t)
 	Result& result = boost::get<Result> (res);
 
 	// Check final x.
-	for (typename F<T>::size_type i = 0; i < result.x.size (); ++i)
+        for (F<functionType_t>::size_type i = 0; i < result.x.size (); ++i)
 	  BOOST_CHECK_CLOSE (result.x[i], ExpectedResult::x[i], check_tol);
 
 	// Check final value.
@@ -210,7 +209,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE (distanceToSphere_problem1, T, functionTypes_t)
 	ResultWithWarnings& result = boost::get<ResultWithWarnings> (res);
 
 	// Check final x.
-	for (typename F<T>::size_type i = 0; i < result.x.size (); ++i)
+        for (F<functionType_t>::size_type i = 0; i < result.x.size (); ++i)
 	  BOOST_CHECK_CLOSE (result.x[i], ExpectedResult::x[i], check_tol);
 
 	// Check final value.
