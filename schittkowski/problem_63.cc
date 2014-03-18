77fdf36f5a4d1a8233a1afa6c9edc9dd9fc8cd97
Benjamin Chrétien <chretien@lirmm.fr>
2014-03-18 23:34:46 +0100

    Add 63rd problem.

---
+++ schittkowski/problem_63.cc
@@ -0,0 +1,216 @@
+// Copyright (C) 2014 by Benjamin Chretien, CNRS.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "common.hh"
+
+namespace roboptim
+{
+  namespace schittkowski
+  {
+    namespace problem63
+    {
+      struct ExpectedResult
+      {
+	static const double f0;
+	static const double x[];
+	static const double fx;
+      };
+      const double ExpectedResult::f0 = 976;
+      const double ExpectedResult::x[] = {3.512118414, 0.2169881741,
+                                          3.552174034};
+      const double ExpectedResult::fx = 961.7151721;
+
+      template <typename T>
+      class F : public GenericDifferentiableFunction<T>
+      {
+      public:
+	ROBOPTIM_DIFFERENTIABLE_FUNCTION_FWD_TYPEDEFS_
+	(GenericDifferentiableFunction<T>);
+
+	explicit F () throw ();
+	void
+	impl_compute (result_t& result, const argument_t& x) const throw ();
+	void
+	impl_gradient (gradient_t& grad, const argument_t& x, size_type)
+	  const throw ();
+      };
+
+      template <typename T>
+      F<T>::F () throw ()
+	: GenericDifferentiableFunction<T>
+	  (3, 1, "1000 - x₀² - 2x₁² - x₂² - x₀x₁ - x₀x₂")
+      {}
+
+      template <typename T>
+      void
+      F<T>::impl_compute (result_t& result, const argument_t& x)
+	const throw ()
+      {
+	result[0] = 1000 - x[0] * x[0] - 2 * x[1] * x[1] - x[2] * x[2]
+	  - x[0] * x[1] - x[0] * x[2];
+      }
+
+      template <>
+      void
+      F<EigenMatrixSparse>::impl_gradient
+      (gradient_t& grad, const argument_t& x, size_type)
+	const throw ()
+      {
+	grad.insert (0) = -2 * x[0] - x[1] - x[2];
+	grad.insert (1) = -4 * x[1] - x[0];
+	grad.insert (2) = -2 * x[2] - x[0];
+      }
+
+      template <typename T>
+      void
+      F<T>::impl_gradient (gradient_t& grad, const argument_t& x, size_type)
+	const throw ()
+      {
+	grad (0) = -2 * x[0] - x[1] - x[2];
+	grad (1) = -4 * x[1] - x[0];
+	grad (2) = -2 * x[2] - x[0];
+      }
+
+      template <typename T>
+      class G : public GenericDifferentiableFunction<T>
+      {
+      public:
+	ROBOPTIM_DIFFERENTIABLE_FUNCTION_FWD_TYPEDEFS_
+	(GenericDifferentiableFunction<T>);
+
+	explicit G () throw ();
+	void
+	impl_compute (result_t& result, const argument_t& x) const throw ();
+	void
+	impl_gradient (gradient_t&, const argument_t&, size_type)
+	  const throw () {}
+	void
+	impl_jacobian (jacobian_t& jac, const argument_t& x)
+	  const throw ();
+      };
+
+      template <typename T>
+      G<T>::G () throw ()
+	: GenericDifferentiableFunction<T>
+	  (3, 2, "8x₀ + 14x₁ + 7x₂ - 56, x₀² + x₁² + x₂² -25")
+      {}
+
+      template <typename T>
+      void
+      G<T>::impl_compute (result_t& result, const argument_t& x)
+	const throw ()
+      {
+	result[0] = 8 * x[0] + 14 * x[1] + 7 * x[2] - 56;
+	result[1] = x[0] * x[0] + x[1] * x[1] + x[2] * x[2] - 25;
+      }
+
+      template <>
+      void
+      G<EigenMatrixSparse>::impl_jacobian
+      (jacobian_t& jac, const argument_t& x) const throw ()
+      {
+	jac.insert (0,0) = 8;
+	jac.insert (0,1) = 14;
+	jac.insert (0,2) = 7;
+
+	jac.insert (1,0) = 2 * x[0];
+	jac.insert (1,1) = 2 * x[1];
+	jac.insert (1,2) = 2 * x[2];
+      }
+
+      template <typename T>
+      void
+      G<T>::impl_jacobian
+      (jacobian_t& jac, const argument_t& x) const throw ()
+      {
+	jac (0,0) = 8;
+	jac (0,1) = 14;
+	jac (0,2) = 7;
+
+	jac (1,0) = 2 * x[0];
+	jac (1,1) = 2 * x[1];
+	jac (1,2) = 2 * x[2];
+      }
+    } // end of namespace problem63.
+  } // end of namespace schittkowski.
+} // end of namespace roboptim.
+
+BOOST_FIXTURE_TEST_SUITE (schittkowski, TestSuiteConfiguration)
+
+BOOST_AUTO_TEST_CASE (schittkowski_problem63)
+{
+  using namespace roboptim;
+  using namespace roboptim::schittkowski::problem63;
+
+  // Tolerances for Boost checks.
+  double f0_tol = 1e-4;
+  double x_tol = 1e-4;
+  double f_tol = 1e-4;
+
+  // Build problem.
+  F<functionType_t> f;
+  solver_t::problem_t problem (f);
+
+  for (F<functionType_t>::size_type i = 0; i < f.inputSize (); ++i)
+    problem.argumentBounds ()[i] = F<functionType_t>::makeLowerInterval (0.);
+
+  boost::shared_ptr<G<functionType_t> > g =
+    boost::make_shared<G<functionType_t> > ();
+
+  solver_t::problem_t::intervals_t intervals;
+  intervals.push_back (G<functionType_t>::makeInterval (0., 0.));
+  intervals.push_back (G<functionType_t>::makeInterval (0., 0.));
+  solver_t::problem_t::scales_t scales (g->outputSize (), 1.);
+
+  problem.addConstraint (g, intervals, scales);
+
+  F<functionType_t>::argument_t x (f.inputSize ());
+  x << 2, 2, 2;
+  problem.startingPoint () = x;
+
+  BOOST_CHECK_SMALL_OR_CLOSE (f (x)[0], ExpectedResult::f0, f0_tol);
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+  // Initialize solver.
+  SolverFactory<solver_t> factory (SOLVER_NAME, problem);
+  solver_t& solver = factory ();
+  OptimizationLogger<solver_t> logger
+    (solver,
+     "/tmp/roboptim-shared-tests/" SOLVER_NAME "/schittkowski/problem-63");
+
+  // Set optional log file for debugging
+  SET_LOG_FILE(solver);
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+  // Compute the minimum and retrieve the result.
+  solver_t::result_t res = solver.minimum ();
+
+  std::cout << f.inputSize () << std::endl;
+  std::cout << problem.function ().inputSize () << std::endl;
+
+  // Display solver information.
+  std::cout << solver << std::endl;
+
+  // Process the result
+  PROCESS_RESULT();
+}
+
+BOOST_AUTO_TEST_SUITE_END ()
